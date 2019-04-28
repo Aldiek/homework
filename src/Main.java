673dd0b80698b7c05bc779b6af48984058d27b18
Mouhsen Ibrahim <mohsen47@hotmail.co.uk>
2019-04-28 16:32:29 +0300

    Final grade

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+// 10 marks
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,53 +10,53 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
-		BlockingQueue q1 = new BlockingQueue(100);
+		BlockingQueue q1 = new BlockingQueue(100); // 1/2 marks
 		BlockingQueue q2 = new BlockingQueue(100);
 
 		FirstThread fg = new FirstThread(q1);
 		Thread th1 = new Thread(fg);
-		
-		SecondThread th2[] = new SecondThread[8];
-		
+
+		SecondThread th2[] = new SecondThread[8]; // 1/2 marks
+
 		ThirdThread TT = new ThirdThread(q2);
-		Thread th3 = new Thread(TT);
-		
-		
-		
-		
+		Thread th3 = new Thread(TT); // 1/2 marks
+
+
+
+
 		th1.start();
 			for(int i=0;i<8;i++) {
 				 th2[i] = new SecondThread(q1,q2);
 				 th2[i].start();
 
-				
+
 			}
-		
 
-		
 
-		
 
-		
 
-		
 
-		
+
+
+
+
+
+
 		th3.start();
-		
 
-		
+
+
 		th1.join();
-		
-		for(int i=0;i<8;i++) {
-			
+
+		for(int i=0;i<8;i++) { // 1/2 marks
+
 			 th2[i].join();
 
-			
-		}
-				
 
-		
+		}
+
+
+
 		th3.join();
 
 		System.out.println("THE END");
@@ -70,9 +71,9 @@ class BlockingQueue {
 	private int counter=0;
 	public synchronized int count() {
 		counter++;
-		
+
 		return counter;
-		
+
 	}
 
 
@@ -109,17 +110,17 @@ class BlockingQueue {
 }
 
 class FirstThread extends Thread {
-	BlockingQueue q1;
+	BlockingQueue q1; // 1/2 marks
 
-	public FirstThread(BlockingQueue q) {
+	public FirstThread(BlockingQueue q) { // 1/2 marks
 		this.q1 = q;
 	}
 
 	@Override
 	public void run() {
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 100; i++) { // 1/2 marks
 			try {
-				q1.add("f" + i + ".txt");
+				q1.add("f" + i + ".txt"); // 1/2 marks
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -132,21 +133,21 @@ class FirstThread extends Thread {
 
 class SecondThread extends Thread {
 	BlockingQueue q1;
-	BlockingQueue q2;
+	BlockingQueue q2; // 1/2 marks
 	private String rawDataFromFile;
 	private String processedData;
 	private String filename;
 
-	public SecondThread(BlockingQueue q1, BlockingQueue q2) {
+	public SecondThread(BlockingQueue q1, BlockingQueue q2) { // 1/2 marks
 		this.q1 = q1;
 		this.q2 = q2;
 	}
 
 	@Override
 	public void run() {
-		while(q2.count()<=100) {
+		while(q2.count()<=100) { // 2 marks
 			try {
-				filename = q1.pop().toString();
+				filename = q1.pop().toString(); // 1/2 marks
 			} catch (InterruptedException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
@@ -159,7 +160,7 @@ class SecondThread extends Thread {
 			}
 			processedData = filename + FileUtils.countchars(rawDataFromFile);
 			try {
-				q2.add(processedData);
+				q2.add(processedData); // 1/2 marks
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -172,18 +173,18 @@ class SecondThread extends Thread {
 }
 
 class ThirdThread extends Thread {
-	BlockingQueue q2;
+	BlockingQueue q2; // 1/2 marks
 	private String processedData;
 
-	public ThirdThread(BlockingQueue q2) {
+	public ThirdThread(BlockingQueue q2) { // 1/2 marks
 		this.q2 = q2;
 	}
 
 	@Override
 	public void run() {
-		for (int i = 0; i < 100;i++) {
+		for (int i = 0; i < 100;i++) { // 1/2 marks
 			try {
-				processedData = (String) q2.pop();
+				processedData = (String) q2.pop(); // 1/2 marks
 			} catch (InterruptedException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
